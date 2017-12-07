7543f38c092de5b9bcb60ee5fde0a17bad4a6cc4
Maksym_Donchenko <maksym_donchenko@epam.com>
2017-12-07 18:07:31 +0200

    via stack and 3 bracket maps: opening / closing / self-closing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 === 1) return false;
+
+    const openToCloseBracketsMap = {};
+    const closeToOpenBracketsMap = {};
+    const selfClosingBracketsMap = {};
+    bracketsConfig.forEach(currBracketPair => {
+        const openingBracket = currBracketPair[0];
+        const closingBracket = currBracketPair[1];
+        if (openingBracket === closingBracket) {
+            selfClosingBracketsMap[openingBracket] = closingBracket;
+            return;
+        }
+        openToCloseBracketsMap[openingBracket] = closingBracket;
+        closeToOpenBracketsMap[closingBracket] = openingBracket;
+    });
+
+    let isCorrectOrder = true;
+    const stackAfterParsing = str.split('').reduce((stack, currChar) => {
+        const isSelfClosing = currChar in selfClosingBracketsMap;
+        const isClosing = currChar in closeToOpenBracketsMap;
+        const isOpening = currChar in openToCloseBracketsMap;
+        const lastInStack = stack[stack.length - 1];
+        const complementaryBracket = getComplementaryBracket(
+            isClosing ? closeToOpenBracketsMap : openToCloseBracketsMap,
+            currChar
+        );
+
+        if (isSelfClosing) {
+            if (lastInStack === currChar) {
+                stack.pop();
+            } else {
+                stack.push(currChar);
+            }
+        } else if (isOpening) {
+            stack.push(currChar);
+        } else if (isClosing) {
+            stack.pop();
+            if (lastInStack !== complementaryBracket
+                || !lastInStack
+                || !complementaryBracket) {
+                isCorrectOrder = false;
+            }
+        }
+        return stack;
+    }, []);
+    return isCorrectOrder && stackAfterParsing.length === 0;
+};
+
+function getComplementaryBracket(bracketsMap, bracket) {
+    return bracketsMap[bracket];
 }
